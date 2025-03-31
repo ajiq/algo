272cb6c6f47f6855854339fd77fb16ae981bc94e
Jufri Azziq Bin Yusoff <61681576+ajiq@users.noreply.github.com>
2025-03-31 13:01:10 +0300

    added calculator.py

---
+++ calculator.py
@@ -0,0 +1,7 @@
+def calculate_sum():
+    a = int(input("Введите первое число: "))
+    b = int(input("Введите второе число: "))
+    print(f"Результат: {a + b}")
+
+if __name__ == "__main__":
+    calculate_sum()
